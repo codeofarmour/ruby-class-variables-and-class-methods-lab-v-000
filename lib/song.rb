76c932446b2200fbdcc96586f0ff8a44a1d1fbae
Terri Riggle <codeofarmour@outlook.com>
2018-06-26 01:01:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,18 @@ class Song
     @@artists.uniq
   end
   
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1 
+      end
+    end 
+    artist_count
+  end
+  
   def self.genre_count
     counts = Hash.new 0 
     @@genres.each do |genre|
@@ -34,11 +46,5 @@ class Song
     counts
   end
   
-  def self.artist_count
-    counts = Hash.new 0 
-    @@artists.each do |artist|
-      counts[artist] += 1 
-    end
-    counts
-  end
+  
 end 
